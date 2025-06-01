bde314e4efb904ef346d2e4e0cd17030f77990ec
Samuel Hammersberg <samuel@loafey.se>
2025-06-01 13:05:30 +0200

    Clean up binary file

--- src/bin.rs
+++ src/bin.rs
@@ -1,54 +1,19 @@
-#![feature(random)]
-
-use std::random::random;
+use std::time::Instant;
 
 fn fib(num: usize) -> usize {
     match num {
         0 => 0,
         1 => 1,
         _ => {
-            let a = zippy::spawn(move || fib(num - 1)).wait();
-            let b = zippy::spawn(move || fib(num - 2)).wait();
-            a + b
+            let a = zippy::spawn(move || fib(num - 1));
+            let b = zippy::spawn(move || fib(num - 2));
+            a.wait() + b.wait()
         }
     }
 }
 
 fn main() {
-    // println!("{}", jobs.into_iter().map(|a| a.wait()).sum::<usize>());
+    let now = Instant::now();
     zippy::spawn(|| fib(20)).wait();
-    println!("After recursive test stats: {:#?}", zippy::get_stats());
-    let mut jobs = Vec::new();
-    for i in 0..100000 {
-        jobs.push(zippy::spawn(move || i));
-    }
-    jobs.into_iter().map(|a| a.wait()).sum::<usize>();
-    println!(
-        "After a lot of linear work stats: {:#?}",
-        zippy::get_stats()
-    );
-
-    let mut jobs = Vec::new();
-    fn panic_job() {
-        let rand = random::<usize>() % 2;
-        if rand == 0 {
-            panic!("OUCH!")
-        }
-    }
-    for _ in 0..100000 {
-        jobs.push(zippy::spawn(panic_job));
-    }
-
-    let mut sum = 0;
-    while let Some(w) = jobs.pop() {
-        if w.try_wait().is_some() {
-            sum += 1;
-        } else {
-            jobs.push(zippy::spawn(panic_job));
-        }
-    }
-    println!(
-        "After crash test stats (sum: {sum}): {:#?}",
-        zippy::get_stats()
-    );
+    println!("took {:?}", now.elapsed())
 }
